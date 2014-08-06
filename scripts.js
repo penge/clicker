55e9508ee89d6d1de817c1a0370b684d38db27b2
Pavel Bucka <pavelbucka@icloud.com>
2014-08-06 19:29:13 +0200

    Define and use GAME object

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $(function() {
+  GAME.init();
 });
 
 var UI = (function() {
@@ -22,6 +23,7 @@ var UI = (function() {
       reset();
       $('#score, #highscore').show();
       $(this).hide();
+      GAME.start();
     });
   }
 
@@ -44,3 +46,163 @@ var UI = (function() {
     updateHighscore: updateHighscore,
   }
 })();
+
+var GAME = (function(UI) {
+  var //var
+    _id        = 0,
+    _score     = 0,
+    _highscore = 0,
+    _circles   = 0,
+    _timeout   = 0,
+    _over      = true;
+   
+  function init() {
+    reset();
+    UI.init();
+  }
+
+  function reset() {
+    _score   = 0;
+    _circles = 0;
+    _timeout = 5000;
+    _over    = true;
+  }
+
+  function incrementScore() {
+    ++_score;
+  }
+
+  function updateHighscore() {
+    _highscore = (_score > _highscore) ? _score : _highscore;
+  }
+
+  function canContinue(gameId) {
+    return !_over && (_id == gameId);
+  }
+
+  function over() {
+    new Audio("over.mp3").play();
+    addManyCircles(_id);
+    reset();
+    UI.over();
+  }
+
+  function addManyCircles(gameId) {
+    for (var i = 0; i < 50; i++) {
+      addCircle(gameId);
+    }
+  }
+
+  function start() {
+    ++_id;
+    _over = false;
+    keepAddingCircles(_id);
+  }
+
+  function keepAddingCircles(gameId) {
+    if (!canContinue(gameId)) {
+      return false;
+    }
+
+    addCircle(gameId);
+    upSpeed();
+    upDifficulty(gameId);
+    setTimeout(function() {
+      keepAddingCircles(gameId);
+    }, _timeout);
+  }
+
+  function addCircle(gameId) {
+    if (!canContinue(gameId)) {
+      return false;
+    }
+    
+    var //var
+      id       = ++_circles,
+      diameter = 180,
+      top      = getRandom(20, $(document.body).height() - diameter),
+      left     = getRandom(20, $(document.body).width() - diameter); 
+
+    $('<div/>', {
+      id: id,
+      class: 'circle',
+      css: {
+        top:    top,
+        left:   left,
+        height: diameter,
+        width:  diameter
+      },
+      click: function() {
+        if (!canContinue(gameId)) {
+          return false;
+        }
+        hitCircle(this);
+      }
+    }).appendTo(document.body).animate({
+      left:   '+=' + diameter / 2,
+      top:    '+=' + diameter / 2,
+      width:  0,
+      height: 0
+    }, {
+      duration: 5000,
+      easing: 'easeOutQuad',
+      step: function(now, fx) {
+        fx.now = parseInt(now);
+      },
+      complete: function() {
+        if (canContinue(gameId)) {
+          over();
+        }
+      }
+    });
+  }
+
+  function hitCircle(circle) {
+    new Audio("hit.mp3").play();
+    $(circle).stop().remove();
+
+    incrementScore();
+    updateHighscore();
+    
+    UI.updateScore(_score);
+    UI.updateHighscore(_highscore);
+  }
+
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function upSpeed() {
+    // Increase speed by decreasing timeout and do it every 2nd circle hit
+    if (_timeout > 1000 && _circles % 2 === 0) {
+      _timeout -= 100;
+    }
+  }
+
+  function upDifficulty(gameId) {
+    // Every 2nd circle, wait 2 seconds, and add another circle
+    if (_circles % 2 === 0) {
+      setTimeout(function() {
+        addCircle(gameId);
+      }, 2000);
+    }
+    
+    // Every 5th circle, wait 4 seconds, and add another circle
+    if (_circles % 5 === 0) {
+      setTimeout(function() {
+        addCircle(gameId);
+      }, 4000);
+    }
+    
+    // Every 8th circle, wait 5 seconds, and start adding circles more often
+    if (_circles % 8 === 0) {
+      setTimeout(keepAddingCircles(gameId), 5000);
+    }
+  }
+
+  return {
+    init:  init,
+    over:  over,
+    start: start,
+  } 
+})(UI);
